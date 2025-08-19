92d88cf451c82104c589cb2b843f27ae99148e74
Binuri Mindula <binuriwagarachchi@gmail.com>
2025-08-19 17:27:49 +0530

    Add blog post feature

---
+++ Bloggie/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,41 @@
+﻿using Bloggie.Models.ViewModels;
+using Bloggie.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bloggie.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagRepository _tagRepository;
+
+        public AdminBlogPostsController(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            //get tags from repository
+            var tags = await _tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(tag => new SelectListItem
+                {
+                    Text = tag.Name,
+                    Value = tag.Id.ToString()
+                   
+                })
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
+        {
+            return RedirectToAction("Add");
+        }
+    }
+}
