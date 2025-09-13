b517f3c71e9800b7d1e37f021a45dce248d53cb5
Binuri Mindula <binuriwagarachchi@gmail.com>
2025-09-13 16:17:33 +0530

    Configure cloudinary

--- Bloggie/Repositories/CloudinaryImageRepository.cs
+++ Bloggie/Repositories/CloudinaryImageRepository.cs
@@ -1,10 +1,42 @@
-﻿namespace Bloggie.Repositories
+﻿using System.Net;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace Bloggie.Repositories
 {
     public class CloudinaryImageRepository: IImageRepository
     {
-        public Task<string> UploadAsync(IFormFile formFile)
+        private readonly IConfiguration _configuration;
+        private readonly Account account;
+
+        public CloudinaryImageRepository(IConfiguration configuration)
+
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
+            account = new Account(
+                _configuration["Cloudinary:CloudName"],
+                _configuration["Cloudinary:ApiKey"],
+                _configuration["Cloudinary:ApiSecret"]
+                );
+        }
+
+        public async Task<string> UploadAsync(IFormFile formFile)
+        {
+            var client = new Cloudinary(account);
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(formFile.FileName, formFile.OpenReadStream()),
+                DisplayName = formFile.FileName
+            };
+
+            var uploadResult = await client.UploadAsync(uploadParams);
+
+            if (uploadResult != null && uploadResult.StatusCode == HttpStatusCode.OK)
+            {
+                return uploadResult.SecureUri.ToString();
+            }
+
+            return null;
         }
     }
 }
