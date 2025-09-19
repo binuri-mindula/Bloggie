b67d1fa3514d53590155a98ed8c5aef8a215f28f
Binuri Mindula <binuriwagarachchi@gmail.com>
2025-09-19 09:53:56 +0530

    Add user roles

---
+++ Bloggie/Data/AuthDbContext.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //seed roles(user,admin,superadmin)
+            var adminRoleId = "2058158a-78b6-4a32-a1bd-a498f85b1c0e";
+            var superAdminRoleId = "c97ec567-e888-4f65-be16-f6c062759046";
+            var userRoleId = "0e229e34-e033-4b55-ac65-72fd74374b57";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "Admin",
+                    Id = adminRoleId,
+                    ConcurrencyStamp = adminRoleId
+                },
+                new IdentityRole
+                {
+                    Name = "SuperAdmin",
+                    NormalizedName = "SuperAdmin",
+                    Id = superAdminRoleId,
+                    ConcurrencyStamp = superAdminRoleId
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "User",
+                    Id = userRoleId,
+                    ConcurrencyStamp = userRoleId
+                },
+
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            //seed superadminuser
+            var superAdminId = "f227666e-7fba-46f8-96c9-a9e39c525131";
+            var superAdminUser = new IdentityUser
+            {
+                UserName = "superAdmin@bloggie.com",
+                Email = "superAdmin@bloggie.com",
+                NormalizedEmail = "superAdmin@bloggie.com".ToUpper(),
+                NormalizedUserName = "superAdmin@bloggie.com".ToUpper(),
+                Id = superAdminId
+            };
+
+            superAdminUser.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(superAdminUser, "SuperAdmin@123");
+
+            builder.Entity<IdentityUser>().HasData(superAdminUser);
+
+            //add all roles to superadminuser
+            var superAdminRoles = new List<IdentityUserRole<string>>
+            {
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRoleId,
+                    UserId = superAdminId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = superAdminRoleId,
+                    UserId = superAdminId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = userRoleId,
+                    UserId = superAdminId
+                },
+            };
+
+            builder.Entity<IdentityUserRole<string>>().HasData(superAdminRoles);
+
+
+        }
+
+
+    }
+    
+
+}
