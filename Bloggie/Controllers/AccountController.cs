0f2d3b297ea35d2ae248d0ed79a5071989a15353
Binuri Mindula <binuriwagarachchi@gmail.com>
2025-10-02 15:56:24 +0530

    Signin and Signout functionality add

--- Bloggie/Controllers/AccountController.cs
+++ Bloggie/Controllers/AccountController.cs
@@ -7,10 +7,12 @@ namespace Bloggie.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public AccountController(UserManager<IdentityUser> userManager)
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
@@ -42,6 +44,34 @@ namespace Bloggie.Controllers
             }
             //show error notification
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            var signInResult =
+                await _signInManager.PasswordSignInAsync(loginViewModel.Username, loginViewModel.Password, false,
+                    false);
+            if (signInResult != null && signInResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            //show errors
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
